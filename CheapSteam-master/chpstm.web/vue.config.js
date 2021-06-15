4a1ea365b7bbd005a96d0ead8465fbf55ce235db
1194326131 <85932433+1194326131@users.noreply.github.com>
2021-06-15 17:24:41 +0800

    Add files via upload

---
+++ CheapSteam-master/chpstm.web/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "transpileDependencies": [
+    "vuetify"
+  ],
+
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = 'Cheap Steam - Steam 市场对比站'
+        return args
+      })
+  },
+
+  outputDir: '../ChpStmScraper.WebApi/wwwroot'
+}
